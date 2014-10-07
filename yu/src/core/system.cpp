3429f6229a284519145494dae99732926ee00092
ianslayer <ianslayer@gmail.com>
2014-10-07 23:58:13 +0800

    windows display

--- yu/src/core/system.cpp
+++ yu/src/core/system.cpp
@@ -1,37 +1,12 @@
 #include "system.h"
 
-#if defined YU_OS_WIN32
-	#define WIN32_LEAN_AND_MEAN
-	#include <windows.h>
-#endif
-
 #include <stdio.h>
 
 namespace yu
 {
 System* gSystem = 0;
 
-#if defined YU_OS_WIN32
-BOOL CALLBACK MyMonitorEnumProc(
-	_In_  HMONITOR hMonitor,
-	_In_  HDC hdcMonitor,
-	_In_  LPRECT lprcMonitor,
-	_In_  LPARAM dwData
-)
-{
-	MONITORINFOEX monInfo;
-	monInfo.cbSize = sizeof(MONITORINFOEX);
 
-	BOOL getMonResult = GetMonitorInfo(hMonitor, &monInfo);
-
-	return getMonResult;
-}
-
-void System::GetSysDisplayInfo()
-{
-	EnumDisplayMonitors(NULL, NULL, MyMonitorEnumProc, 0);
-}
-#endif
 	
 void InitSystem()
 {
